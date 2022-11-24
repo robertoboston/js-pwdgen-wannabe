45673ee7d389d232b20bb9bee6fc184d8a644bb5
roberto.boston <roberto.bozzo97@tiscali.it>
2022-11-24 15:44:03 +0100

    excercise done

--- script.js/myscript.js
+++ script.js/myscript.js
@@ -1,1 +1,19 @@
-let name_user=prompt('digita qui il tuo nome')
+//1
+let name_user=prompt('digita qui il tuo nome');
+console.log(name_user)
+//2
+let surname_user=prompt('digita qui il tuo cognome')
+console.log(surname_user)
+//3
+let color_user=prompt('digita qui il tuo colore preferito')
+console.log(color_user)
+//4
+  4.1
+  let content_tag = document.getElementById('password').innerHTML;
+  4.2
+  let string=`${name_user} ${surname_user} ${color_user} ${content_tag}`;
+  4.3
+  document.getElementById('password').innerHTML=string
+  console.log(string)
+
+
